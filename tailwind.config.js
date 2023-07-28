8b9fcbdd3fbc1a57d6a18876c51df42ea0993c6c
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2023-07-28 08:41:14 -0400

    refact: Migrate styles to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tlw-primary': '#2980b9',
+        'tlw-dark': '#282c34',
+        'tlw-light': '#ecf0f1',
+      },
+      fontSize: {
+        base: 'calc(10px + 2vmin)',
+      },
+    },
   },
   plugins: [],
 };
